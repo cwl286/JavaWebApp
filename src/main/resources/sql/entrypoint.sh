68d35cb5bce16728025e00fc6c9d565efe34d5e1
L <cheungwailokhk@gmail.com>
2022-10-20 23:57:10 +0100

    Update mysql statement
    Update mysql statement of parameters

--- src/main/resources/sql/entrypoint.sh
+++ src/main/resources/sql/entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-database=TestDB
-password=Admin_password123
 wait_time=2s
 
-# wait for SQL Server to come up
-echo importing data will start in $wait_time...
+# wait for Mysql Server to come up
+echo MySql will start in $wait_time...
 sleep $wait_time
-echo importing data...
+echo Loading MySql...
 
 
 #Example
 # /var/lib/mysql/mysql > source \home\user\Desktop\test.sql;
-
-# run the init script to create the DB
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./init.sql
-
+mysql -uroot --password=Password -e "CREATE DATABASE TestDB"
+mysql -uroot --password=Password -e "USE TestDB"
